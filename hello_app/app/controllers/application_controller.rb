1717c1081f3b3ae4c31d3a9624386921608a7ebb
canaanmckenzie <cjmckenz@uvm.edu>
2017-08-20 12:16:48 -0400

    hello world controllers added

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+#Render HTML Hello World Text to view
+  def hello
+  	render html: "hello, world"
+  end
+
+  def hola
+  	render html: "¡hola, mundo"
+  end
+
+  def goodbye
+  	render html: "goodbye!"
+  end
+  
 end
